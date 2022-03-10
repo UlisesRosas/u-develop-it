7d5b9ee6f2d9f71e5f5ce3ad8f50d2be2db716ad
ulisMac <rosas_u@ymail.com>
2022-03-09 16:37:26 -0800

    madea delete and add candidate table query

--- server.js
+++ server.js
@@ -21,18 +21,48 @@ const db = mysql.createConnection(
 
 
 // querys the database to see if we get the candidates information
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//   });
+
+// GET a single candidate query. Read functionality 
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
   });
 
+// Delete a candidate query. we use 'result' instead of row in the params. we use result for any query making changes
+// the ? ia s place holder for any value in this case the 1 but can enter multiple values to repete the query on multiple IDs
+// this '?' place holder can also protect the database from an injection attack
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
 
-// function that starts server. Must be at the bottom
-// sinceits a catch all make sure that this function stays near the bottom  
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+
+// Create a candidate query.
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
+  });
+
+  // function that starts server. Must be at the bottom
+// sinceits a catch all make sure that this function stays near the bottom  
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
